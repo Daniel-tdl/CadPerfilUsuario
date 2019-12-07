5b0a298dadfd07d805eb48ca84fe164156635fc2
Daniel_jr <daniel@extradigital.com.br>
2019-12-07 14:39:20 -0300

    Criando autenticação via jwt (token) API
    Apricando nas rotas a autenticação

--- backend/config/routers.js
+++ backend/config/routers.js
@@ -4,15 +4,19 @@ module.exports = app => {
     app.post('/validarToken', app.api.login.validarToken)
 
     app.route('/usuarios')
+        .all(app.config.passaport.authenticate())
         .post(app.api.usuario.save)
         .get(app.api.usuario.get)
 
     app.route('/usuarios/:id')
+    .all(app.config.passaport.authenticate())
         .put(app.api.usuario.save)
 
     app.route('/usuarios/excluir/:id')
+        .all(app.config.passaport.authenticate())
         .delete(app.api.usuario.excluir)
 
     app.route('/usuarios/getUsuarioById/:id')
+        .all(app.config.passaport.authenticate())
         .get(app.api.usuario.getUsuarioById)
 }
